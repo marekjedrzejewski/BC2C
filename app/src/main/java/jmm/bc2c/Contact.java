5def834872082b459d4686f105b3ba063fb19430
marek <mjedrzejewski92@gmail.com>
2016-01-06 15:44:23 +0100

    Add Contact class with static CreateContactIntent method

---
+++ app/src/main/java/jmm/bc2c/Contact.java
@@ -0,0 +1,24 @@
+package jmm.bc2c;
+
+import android.content.Intent;
+import android.provider.ContactsContract;
+
+/**
+ * Created by marek on 06.01.16.
+ */
+public class Contact {
+
+    // returns intent that has to be started
+    public static Intent CreateContactIntent(String name, String phonenumber) {
+        // Creates a new Intent to insert a contact
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        // Sets the MIME type to match the Contacts Provider
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, name)
+              .putExtra(ContactsContract.Intents.Insert.PHONE, phonenumber);
+
+        return intent;
+    }
+
+}
