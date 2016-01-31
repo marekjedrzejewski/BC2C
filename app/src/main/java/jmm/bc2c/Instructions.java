83a14c6df49e62c1c82aa071021a44aa1adbe019
marek <mjedrzejewski92@gmail.com>
2016-01-31 17:30:10 +0100

    Fix Instructions background color

--- app/src/main/java/jmm/bc2c/Instructions.java
+++ app/src/main/java/jmm/bc2c/Instructions.java
@@ -8,6 +8,24 @@ import android.widget.Toast;
 public class Instructions extends AppCompatActivity {
 
     WebView webView;
+    String instructions = "<html><head><style>" +
+            "body {" +
+            "background-color: #222;" +
+            "color: #DDD;" +
+            "}" +
+            "</style></head>" +
+            "<body>" +
+            "<p>Follow these instructions to get the best results.</p>" +
+            "<h2>Camera position</h2>" +
+            "<p>Camera should look straight at the surface. Change of perspective" +
+            " or rotation may lead to weird results. </p>" +
+            "<h2>Light</h2>" +
+            "<p>It's best to take photograph in good lighting conditions to avoid" +
+            " extensive noise.</p>" +
+            "<h2>Contrast</h2>" +
+            "<p>If text is embossed or otherwise hard to read on the photo" +
+            " it probably also won't be readable by app.</p>" +
+            "</body></html>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +33,9 @@ public class Instructions extends AppCompatActivity {
         setContentView(R.layout.activity_instructions);
 
         webView = (WebView)findViewById(R.id.instructions);
-        String instructions = "<html><head><style>" +
-                         "body {" +
-                         "background-color: #222;" +
-                         "color: #DDD;" +
-                         "}" +
-                         "</style></head>" +
-                         "<body>" +
-                         "<p>Follow these instructions to get the best results.</p>" +
-                         "<h2>Camera position</h2>" +
-                         "<p>Camera should look straight at the surface. Change of perspective" +
-                         " or rotation may lead to weird results. </p>" +
-                         "<h2>Light</h2>" +
-                         "<p>It's best to take photograph in good lighting conditions to avoid" +
-                         " extensive noise.</p>" +
-                         "<h2>Contrast</h2>" +
-                         "<p>If text is embossed or otherwise hard to read on the photo" +
-                         " it probably also won't be readable by app.</p>" +
-                         "</body></html>";
+
+        webView.getSettings();
+        webView.setBackgroundColor(0x212121);
         webView.loadData(instructions, "text/html", null);
 
         Toast.makeText(this, "Press back to close this screen", Toast.LENGTH_SHORT).show();
