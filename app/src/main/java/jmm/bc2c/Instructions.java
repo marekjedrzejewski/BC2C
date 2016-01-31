9204b5cf9e592a16f336e7c4ddc4d731b943bc80
marek <mjedrzejewski92@gmail.com>
2016-01-31 12:40:10 +0100

    Add instructions

--- app/src/main/java/jmm/bc2c/Instructions.java
+++ app/src/main/java/jmm/bc2c/Instructions.java
@@ -16,6 +16,16 @@ public class Instructions extends AppCompatActivity {
         webView = (WebView)findViewById(R.id.instructions);
         String instructions = "<html><body>" +
                          "<b>Press back to close this screen</b>" +
+                         "<p>Follow these instructions to get the best results.</p>" +
+                         "<h2>Camera position</h2>" +
+                         "<p>Camera should look straight at the surface. Change of perspective" +
+                         " or rotation may lead to weird results. </p>" +
+                         "<h2>Light</h2>" +
+                         "<p>It's best to take photograph in good lighting conditions to avoid" +
+                         " extensive noise.</p>" +
+                         "<h2>Contrast</h2>" +
+                         "<p>If text is embossed or otherwise hard to read on the photo" +
+                         " it probably also won't be readable by app.</p>" +
                          "</body></html>";
         webView.loadData(instructions, "text/html", null);
 
