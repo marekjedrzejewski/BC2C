e1771d26920837e886f8d569076cd367bc8b01c9
marek <mjedrzejewski92@gmail.com>
2016-02-01 14:59:20 +0100

    Greyscale + binarize performed by leptonica

--- app/src/main/java/jmm/bc2c/ImagePrep.java
+++ app/src/main/java/jmm/bc2c/ImagePrep.java
@@ -2,32 +2,52 @@ package jmm.bc2c;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+
+import com.googlecode.leptonica.android.AdaptiveMap;
+import com.googlecode.leptonica.android.Binarize;
+import com.googlecode.leptonica.android.Convert;
+import com.googlecode.leptonica.android.Pix;
+import com.googlecode.leptonica.android.ReadFile;
+import com.googlecode.leptonica.android.Scale;
+import com.googlecode.leptonica.android.WriteFile;
 
 /**
  * Created by marek on 25.01.16.
  */
 public class ImagePrep {
     public static Bitmap Prepare(Bitmap image) {
-        return toGrayscale(image);
+        return binarize(toGrayscale(image));
     }
 
-    public static Bitmap toGrayscale(Bitmap image)
-    {
-        int height = image.getHeight();
-        int width = image.getWidth();
-        Bitmap imgGreyscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//    public static Bitmap toGrayscale(Bitmap image)
+//    {
+//        int height = image.getHeight();
+//        int width = image.getWidth();
+//        Bitmap imgGreyscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//
+//        Canvas canvas = new Canvas(imgGreyscale);
+//        Paint paint = new Paint();
+//        ColorMatrix colorMatrix = new ColorMatrix();
+//        colorMatrix.setSaturation(0);
+//        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+//        paint.setColorFilter(filter);
+//        canvas.drawBitmap(image, 0, 0, paint);
+//        return imgGreyscale;
+//    }
 
-        Canvas canvas = new Canvas(imgGreyscale);
-        Paint paint = new Paint();
-        ColorMatrix colorMatrix = new ColorMatrix();
-        colorMatrix.setSaturation(0);
-        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
-        paint.setColorFilter(filter);
-        canvas.drawBitmap(image, 0, 0, paint);
-        return imgGreyscale;
+
+    public static Bitmap toGrayscale(Bitmap image) {
+        Pix imagepix = ReadFile.readBitmap(image);
+        imagepix = Convert.convertTo8(imagepix);
+        return WriteFile.writeBitmap(imagepix);
+    }
+
+    public static Bitmap binarize(Bitmap image) {
+        Pix imagepix = ReadFile.readBitmap(image);
+        imagepix = Binarize.otsuAdaptiveThreshold(imagepix);
+        return WriteFile.writeBitmap(imagepix);
     }
 }
